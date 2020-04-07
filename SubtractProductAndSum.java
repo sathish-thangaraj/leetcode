9368b2805097be8439f2980338303373c000a08d
Sathish <38915683+sathish-thangaraj@users.noreply.github.com>
2020-04-07 18:18:20 -0400

    1281. Subtract the Product and Sum of Digits of an Integer
    https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/

---
+++ SubtractProductAndSum.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class SubtractProductAndSum {
+
+    public static void main(String[] args) {
+        int input = 234;
+        System.out.println(subtractProductAndSum(input));
+    }
+
+    public static int subtractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+
+        while (n > 0) {
+            int remainder = n % 10;
+            sum += remainder;
+            product *= remainder;
+            n = n / 10;
+        }
+
+        return product - sum;
+    }
+}
+}
